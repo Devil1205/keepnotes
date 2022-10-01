1c2ffd2ddc44959d0821fcb4858a3982dbc2b069
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2022-10-01 23:56:26 +0530

    Pagination fixed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
 
   const [localStorageArray, setLocalStorageArray] = useState([]);
 
+  const [page, setPage] = useState(0);
+
+  const updateSetPage = (element) => {
+    setPage(element);
+  }
+
   const createNote = async () => {
     let title = document.querySelector('.addNote input').value;
     let description = document.querySelector('.addNote textarea').value;
@@ -19,29 +25,32 @@ function App() {
     else {
       let temp = await JSON.parse(localStorage.getItem("note"));
       temp.push({ sno: Object.keys(temp).length, title: title, description: description });
+      if (temp.length % 6 === 1)
+        setPage(page + 1);
       setLocalStorageArray(temp);
       localStorage.setItem("note", JSON.stringify(temp));
       console.log(localStorage.getItem("note"));
     }
   }
-  
+
   const onDelete = (index) => {
     let temp = JSON.parse(localStorage.getItem("note"));
     temp.splice(index, 1);
+    if (temp.length === page*6)
+      setPage(page - 1);
     for (let i = index; i < Object.keys(temp).length; i++) {
       temp[i].sno -= 1;
     }
     setLocalStorageArray(temp);
     localStorage.setItem("note", JSON.stringify(temp));
     console.log(localStorage.getItem('note'));
-    // console.log(index);
   }
 
   return (
     <div>
       <Navbar />
       <AddNote createNote={createNote} />
-      <Notes onDelete={onDelete} createNote={createNote} />
+      <Notes onDelete={onDelete} createNote={createNote} updateSetPage={updateSetPage} page={page} />
     </div>
   );
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { useState } from 'react'
 import NotesItem from './NotesItem'
 
 function Notes(props) {
 
-    const [page, setPage] = useState(0);
-
     const nextClick = ()=>{
-        setPage(page+1);
+        props.updateSetPage(props.page+1);
     }
-
+    
     const prevClick = ()=>{
-        setPage(page-1);
+        props.updateSetPage(props.page-1);
     }
 
     return (
@@ -19,7 +16,7 @@ function Notes(props) {
             <h1 className='text-center my-4'>Your Notes</h1>
             <div className="row">
                 {localStorage.getItem("note") !== null && JSON.parse(localStorage.getItem("note")).map((note, index) => {
-                    if(index<page*6+6 && index>=page*6)
+                    if(index<props.page*6+6 && index>=props.page*6)
                     return (
                         <div className="col-xl-3 col-lg-4 col-md-6 col-12 my-2" key={index}>
                             <NotesItem note={note} onDelete={() => { props.onDelete(note.sno) }} />
@@ -29,8 +26,8 @@ function Notes(props) {
                 })}
             </div>
             <div className="container d-flex my-4">
-                <button className='btn btn-success' disabled={page===0} onClick={prevClick}>Previous</button>
-                <button className='btn btn-success' disabled={Math.floor((Object.keys(JSON.parse(localStorage.getItem('note'))).length-1)/6)===page} onClick={nextClick} style={{ marginLeft: "auto" }}>Next</button>
+                <button className='btn btn-success' disabled={props.page===0} onClick={prevClick}>Previous</button>
+                <button className='btn btn-success' disabled={Math.floor((Object.keys(JSON.parse(localStorage.getItem('note'))).length-1)/6)===props.page} onClick={nextClick} style={{ marginLeft: "auto" }}>Next</button>
             </div>
         </div>
     )

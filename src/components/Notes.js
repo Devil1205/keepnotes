409fcafe52e01a497963645175e7e25f7e6cffaf
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2022-10-01 23:28:20 +0530

    Pagination

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,24 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 import NotesItem from './NotesItem'
 
 function Notes(props) {
 
+    const [page, setPage] = useState(0);
+
+    const nextClick = ()=>{
+        setPage(page+1);
+    }
+
+    const prevClick = ()=>{
+        setPage(page-1);
+    }
 
     return (
-        <div className='container py-4'>
-            <h1 className='text-center'>Your Notes</h1>
+        <div className='container my-4'>
+            <h1 className='text-center my-4'>Your Notes</h1>
             <div className="row">
                 {localStorage.getItem("note") !== null && JSON.parse(localStorage.getItem("note")).map((note, index) => {
+                    if(index<page*6+6 && index>=page*6)
                     return (
-                        <div className="col-xl-3 col-lg-4 col-md-6 col-12 m-2" key={index}>
+                        <div className="col-xl-3 col-lg-4 col-md-6 col-12 my-2" key={index}>
                             <NotesItem note={note} onDelete={() => { props.onDelete(note.sno) }} />
                         </div>
                     )
+                    return null;
                 })}
             </div>
-            <div className="container d-flex">
-                <button className='btn btn-success'>Previous</button>
-                <button className='btn btn-success' style={{ marginLeft: "auto" }}>Next</button>
+            <div className="container d-flex my-4">
+                <button className='btn btn-success' disabled={page===0} onClick={prevClick}>Previous</button>
+                <button className='btn btn-success' disabled={Math.floor((Object.keys(JSON.parse(localStorage.getItem('note'))).length-1)/6)===page} onClick={nextClick} style={{ marginLeft: "auto" }}>Next</button>
             </div>
         </div>
     )

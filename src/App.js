669d785b9ef2df7daa5095cdf312032ae1f7856f
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2022-10-02 20:22:20 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
   const createNote = async () => {
     let title = document.querySelector('.addNote input').value;
     let description = document.querySelector('.addNote textarea').value;
-    if (JSON.parse(localStorage.getItem('note')).length === 0) {
+    if (localStorage.getItem('note')===null || JSON.parse(localStorage.getItem('note')).length === 0) {
       setLocalStorageArray(localStorageArray.push({ sno: 0, title: title, description: description }));
       localStorage.setItem("note", JSON.stringify(localStorageArray));
       console.log(localStorage.getItem('note'));
